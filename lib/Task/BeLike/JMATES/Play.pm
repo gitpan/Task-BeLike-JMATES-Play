6d4894bc5f37f13698e664fa94cb754e21d8cdb5
Jeremy Mates <jmates@cpan.org>
2013-06-02 16:57:50 +0000

    Import of JMATES/Task-BeLike-JMATES-Play-0.01 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-JMATES-Play
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JMATES/Task-BeLike-JMATES-Play-0.01.tar.gz
    gitpan-cpan-author:       JMATES
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/JMATES/Play.pm
@@ -0,0 +1,50 @@
+package Task::BeLike::JMATES::Play;
+
+use 5.010000;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Task::BeLike::JMATES::Play - modules used by JMATES (not for work)
+
+=head1 SYNOPSIS
+
+  # and likely also set in your ~/.cpan/CPAN/MyConfig.pm:
+  #   'prerequisites_policy' => q[follow],
+  export PERL_MM_USE_DEFAULT=1
+
+  # install for whatever Perl cpan + environment points to
+  cpan Task::BeLike::JMATES::Play
+
+=head1 DESCRIPTION
+
+Modules used by JMATES not for work (that is, for fun).
+
+=head1 HISTORY
+
+See the git repository, and documentation on how the (public) git
+history is minimized in L<Task::BeLike::JMATES>.
+
+=head1 SEE ALSO
+
+L<http://github.com/thrig/Task-BeLike-JMATES-Play>,
+L<Task::BeLike::JMATES>, L<Task::BeLike::JMATES::WorkWorkWork>
+
+=head1 AUTHOR
+
+Jeremy Mates, E<lt>jmates@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by Jeremy Mates
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself, either Perl version 5.16 or, at
+your option, any later version of Perl 5 you may have available.
+
+=cut
